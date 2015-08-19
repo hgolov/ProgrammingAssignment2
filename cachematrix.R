1e2b1100b7cb97801c4679825044ba3b58514974
Hadassa Golovenshtiz <hgolov@gmail.com>
2015-08-19 20:09:04 +0300

    initial functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,20 @@
 ## functions do
 
 ## Write a short comment describing this function
+##Accept information to create a matrix, instaniate inverse
+##to null. Some getters & setters are included
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(data = NA, cols = 1, rows = 1){
+        x <<- matrix(data, nrow=rows, ncol=cols)
+        inverse <<- NULL
+    }
+    get <-function() x
+    setinverse <- function(inv){ inverse <<- inv}
+    getinverse <- function()inverse
+    list(set = set, get = get, setinverse= setinverse,
+          getinverse=getinverse)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse
+    if(!is.null(inv)){
+      message("getting cached data")
+      return (inv)
+    }
+    data <- x$get
+    inv <-solve(data)
+    x$setinverse(inv)
+    inv
 }
